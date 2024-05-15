71a8e80ce2cc17c7cde54961744cf90bca8d07b2
JacobLow <low_zhang_wei@hotmail.com>
2024-05-15 13:44:05 +0800

    Mortagage Calculator Changes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,34 +1,38 @@
-import React from 'react';
-import {BrowserRouter, Routes, Route} from "react-router-dom"
-import Navbar from "./Components/Navbar"
-import "./App.css"
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "./Components/Navbar";
+import "./App.css";
 // Components
 
 // Pages
-import Home from "./Pages/Home"
-import Buy from "./Pages/User/Buy"
-import Login from "./Pages/Login"
-import Register from "./Pages/Register"
-import AgentsHomePage from "./Pages/Agents/AgentsHome"
+import Home from "./Pages/Home";
+import Buy from "./Pages/User/Buy";
+import Login from "./Pages/Login";
+import Register from "./Pages/Register";
+import MortgageCalculator from "./Pages/MortgageCalculator";
+import AgentsHomePage from "./Pages/Agents/AgentsHome";
 
 function App() {
-
-    return (
-        <>
-            <div>
-                <BrowserRouter>
-                    <Navbar></Navbar>
-                    <Routes>
-                        <Route path="/" element={<Home/>}/>
-                        <Route path="/buy" element={<Buy/>}/>
-                        <Route path="/login" element={<Login/>}/>
-                        <Route path="/register" element={<Register/>}/>
-                        <Route path="/agents" element={<AgentsHomePage/>}/>
-                    </Routes>
-                </BrowserRouter>
-            </div>
-        </>
-    );
+  return (
+    <>
+      <div>
+        <BrowserRouter>
+          <Navbar></Navbar>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/buy" element={<Buy />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route
+              path="/mortgage-calculator"
+              element={<MortgageCalculator />}
+            />
+            <Route path="/agents" element={<AgentsHomePage />} />
+          </Routes>
+        </BrowserRouter>
+      </div>
+    </>
+  );
 }
 
 export default App;
