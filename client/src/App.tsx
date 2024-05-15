35793c81bba98d6ea7306cfe9da9c78b2431b3f4
JacobLow <low_zhang_wei@hotmail.com>
2024-05-15 13:44:05 +0800

    Mortagage Calculator Changes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,32 +1,36 @@
-import React from 'react';
-import {BrowserRouter, Routes, Route} from "react-router-dom"
-import Navbar from "./Components/Navbar"
-import "./App.css"
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "./Components/Navbar";
+import "./App.css";
 // Components
 
-import Card from "./Components/Card"
+import Card from "./Components/Card";
 
 // Pages
-import Home from "./Pages/Home"
-import Login from "./Pages/Login"
-import Register from "./Pages/Register"
+import Home from "./Pages/Home";
+import Login from "./Pages/Login";
+import Register from "./Pages/Register";
+import MortgageCalculator from "./Pages/MortgageCalculator";
 
 function App() {
-
-    return (
-        <>
-            <div>
-                <BrowserRouter>
-                    <Navbar></Navbar>
-                    <Routes>
-                        <Route path="/" element={<Home/>}/>
-                        <Route path="/login" element={<Login/>}/>
-                        <Route path="/register" element={<Register/>}/>
-                    </Routes>
-                </BrowserRouter>
-            </div>
-        </>
-    );
+  return (
+    <>
+      <div>
+        <BrowserRouter>
+          <Navbar></Navbar>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route
+              path="/mortgage-calculator"
+              element={<MortgageCalculator />}
+            />
+          </Routes>
+        </BrowserRouter>
+      </div>
+    </>
+  );
 }
 
 export default App;
