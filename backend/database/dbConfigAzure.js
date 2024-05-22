50a40a0c7968e1cde22578137b0a590a8d1d237c
JacobLow <low_zhang_wei@hotmail.com>
2024-05-23 00:06:19 +0800

    changed DB

--- backend/database/dbConfigAzure.js
+++ backend/database/dbConfigAzure.js
@@ -2,7 +2,8 @@ const config = {
     host: 'real-estate-db.mysql.database.azure.com',       
     user: 'admin314',           
     password: 'Software314!',   
-    database: 'real_estate_platform'  
+    database: 'real_estate_schema'
+    // database: 'real_estate_platform'  
 };
 
 export default config;
