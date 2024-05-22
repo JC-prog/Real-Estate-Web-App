f781f93810e265e5d762c884dfea66b928d14616
Jeffrey Chong <jeffrey.chong28@gmail.com>
2024-05-22 23:59:25 +0800

    feat: add admin dashboard

---
+++ client/src/Components/AgentListingManagementCard.tsx
@@ -0,0 +1,84 @@
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { AxiosResponse, AxiosError } from 'axios';
+import api from '../api/loginApi';
+import "./AgentListingManagementCard.css";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+// Interface
+interface Property {
+    propertyId: string;
+    propertyName: string;
+    propertyAddress: string;
+    propertyStatus: string;
+    price: number;
+}
+
+interface PropertyTableProps {
+    data: Property[];
+}
+
+const AgentListingManagementCard: React.FC<PropertyTableProps> = ({ data = [] }) => {
+    const [refresh, setRefresh] = useState(false);
+    const [properties, setProperties] = useState<Property[]>([]);
+    const navigate = useNavigate();
+
+    if (data.length === 0) {
+        return <p>No properties available.</p>;
+    }
+
+    const viewProperty = async (propertyId: string) => {
+        const propertyData = { propertyId };
+
+        navigate(`/property/${propertyId}`);
+    }
+
+    const formatPrice = (price: number | string) => {
+        const priceNumber = Number(price);
+    
+        if (isNaN(priceNumber)) {
+            console.error('Price is not a valid number:', price);
+            return '0.00'; // Return a default formatted price for invalid numbers
+        }
+    
+        const formattedPrice = priceNumber.toLocaleString('en-US', {
+            minimumFractionDigits: 2,
+            maximumFractionDigits: 2,
+        });
+    
+        return formattedPrice;
+    }
+
+    return (
+        <>
+            {data.map((property) => (
+               <div className="listing-container">
+                    <div className="listing-picture">
+                        <img src={"/HousePic.jpg"} />
+                    </div>
+
+                    <div className="listing-details">
+                        <div className="listing-name">
+                            <h1>{ property.propertyName }</h1>
+
+                            <div className="listing-description">
+                                <h2>Description</h2>
+                                <p>{ property.propertyAddress }</p>
+                                <p>$ { formatPrice(property.price) }</p>
+                            </div>
+                        </div>
+
+                        <div className="property-management-buttons">
+                            <button className="property-view-btn" onClick={() => viewProperty(property.propertyId)}>View</button>
+                            <button className="property-edit-btn">Edit</button>
+                            <button className="property-remove-btn">Remove</button>
+                        </div>
+                    </div>
+                </div>
+            ))}
+        </>      
+    );
+};
+
+export default AgentListingManagementCard;
---
+++ client/src/Pages/Agents/AgentDashboard.tsx
@@ -0,0 +1,96 @@
+import React, { useEffect, useState } from 'react';
+import { useParams, useNavigate } from "react-router-dom";
+import AgentListingManagementCard from "../../Components/AgentListingManagementCard";
+import './AgentDashboard.css';
+import api from '../../api/loginApi';
+
+// Interface
+interface Property {
+    propertyId: string;
+    propertyName: string;
+    propertyAddress: string;
+    propertyStatus: string;
+    price: number;
+}
+
+interface ApiResponseProperties {
+    results: any[];
+}
+
+const getCookieValue = (name: string): string | undefined => {
+    const value = `; ${document.cookie}`;
+    const parts = value.split(`; ${name}=`);
+    if (parts.length === 2) return parts.pop()?.split(';').shift();
+    return undefined;
+};
+
+const AgentDashboard = () => {
+    const [userId, setUserId] = useState('');
+    const [properties, setProperties] = useState<Property[]>([]);
+    const { propertiesId } = useParams();
+
+    useEffect(() => {
+        // Check if the user is logged in by checking local storage
+        const token = getCookieValue('token');
+
+        const response = api.get('api/auth/check-auth', {
+            params: {
+              token: token
+            }
+        }).then(response => {
+            console.log(response.data.userId);
+            setUserId(response.data.userId);
+        });
+    }, []);
+
+    const fetchProperties = async () => {
+
+        try {
+            const response: any = await api.get(`api/properties/properties-agent`, {
+                params: {
+                    agentId : `${ userId }`
+                }
+            });
+
+            console.log('API response:', response);
+
+            console.log(response.data);
+
+            // Map the response data to match the propertys interface
+            const mappedProperties: Property[] = response.data.results.map((item:any) => ({
+                propertyId: item.propertyId,
+                propertyName: item.propertyName,
+                propertyAddress: item.propertyAddress,
+                propertyStatus: item.propertyStatus,
+                price: item.price
+            }));
+
+            console.log(properties);
+
+            setProperties(mappedProperties);
+
+            
+
+        } catch (error) {
+            console.error('Failed to fetch property data:', error);
+           
+        }
+    };
+
+  return (
+    <>
+        <div className="agent-dashboard-container">
+            <h1>AgentDashboard</h1>
+            <div className="section">
+                <h2>Listing Management</h2>
+                <div className="button-container">
+                    <button className="agent-btn" onClick={fetchProperties}>View Listings</button>
+                </div>
+                <AgentListingManagementCard data={properties} />
+            </div>
+        </div>
+    </>
+  )
+}   
+
+export default AgentDashboard
