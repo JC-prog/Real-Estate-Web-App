d3f9df1631b4d064ba41356151711bbeb316be3c
muhammadhaziqAP <muhammad.haziq@asiapac.com.sg>
2024-05-23 23:32:03 +0800

    rename popupbutton

--- client/src/Pages/Agents/AgentProfile.tsx
+++ client/src/Pages/Agents/AgentProfile.tsx
@@ -43,7 +43,7 @@ const AgentProfile: React.FC = () => {
     const [properties, setProperties] = useState<Property[]>([]);
     const [loading, setLoading] = useState<boolean>(true);
     const [error, setError] = useState<string | null>(null);
-    const [buttonPopup, setButtonPopup] = useState(false);
+    const [buttonPopup, rateReviewPopup] = useState(false);
     const [rating, setRating] = useState<number | ''>('');
     const [review, setReview] = useState<string>('');
     const navigate = useNavigate();
@@ -138,7 +138,7 @@ const AgentProfile: React.FC = () => {
                     autoClose: 2000,
                 });
 
-                setButtonPopup(false);
+                rateReviewPopup(false);
 
             } else {
                 // Handle error response
@@ -178,9 +178,9 @@ const AgentProfile: React.FC = () => {
                             </div>
 
                             <div className="profile-buttons">
-                                <button onClick={() => setButtonPopup(true)}>Rate Agent</button>
+                                <button onClick={() => rateReviewPopup(true)}>Rate Agent</button>
 
-                                <Popup trigger={buttonPopup} setTrigger={setButtonPopup}>
+                                <Popup trigger={buttonPopup} setTrigger={rateReviewPopup}>
                                     <h2>Review Agent</h2>
                                     <form onSubmit={submitReview}>
                                         <label>
@@ -204,7 +204,7 @@ const AgentProfile: React.FC = () => {
                                         <br />
                                         <div className="button-group">
                                         <button type="submit">Submit</button>
-                                        <button type="button" className="close-btn" onClick={() => setButtonPopup(false)}>Close</button>
+                                        <button type="button" className="close-btn" onClick={() => rateReviewPopup(false)}>Close</button>
                                         </div>
                                     </form>
                                 </Popup>
--- client/src/Pages/PropertyDetailsPage.tsx
+++ client/src/Pages/PropertyDetailsPage.tsx
@@ -21,37 +21,31 @@ const PropertyDetailsPage: React.FC = () => {
             src="/apartment-sample.jpg"
             alt="property"
           />
-
       </div>
-      <div className="title">
-          <h1 className="heading">{propertyDetails.propertyName}</h1>
-          <p className="address">{propertyDetails.propertyAddress}</p>
-      </div>
-          <p>{propertyDetails.propertyType}</p>
-          <p>{propertyDetails.numberOfRooms}</p>
-          <p>{propertyDetails.area}</p>
-          <p>{propertyDetails.tenure}</p>
-          <p>{propertyDetails.status}</p>
-          <p>{propertyDetails.pricePerSquareFeet}</p>
-          <p>{propertyDetails.price}</p>
-          <p>{formattedListingDate}</p>
-          <p className="About"> About This Property: Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
-              sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut 
-              enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi 
-              ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit 
-              in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur 
-              sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt 
-              mollit anim id est laborum
-          </p>
-
-      <div className="property-details">
-        <div className="details">
-
+      <div className="info-wrapper">
+        <div className="title">
+            <h1 className="heading">{propertyDetails.propertyName}</h1>
+            <p className="address">{propertyDetails.propertyAddress}</p>
         </div>
 
+        <p>{propertyDetails.propertyType}</p>
+        <p>{propertyDetails.numberOfRooms}</p>
+        <p>{propertyDetails.area}</p>
+        <p>{propertyDetails.tenure}</p>
+        <p>{propertyDetails.status}</p>
+        <p>{propertyDetails.pricePerSquareFeet}</p>
+        <p>{propertyDetails.price}</p>
+        <p>{formattedListingDate}</p>
 
       </div>
-
+      <p className="About"> About This Property: Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+            sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut 
+            enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi 
+            ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit 
+            in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur 
+            sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt 
+            mollit anim id est laborum
+        </p>
     </div>
   );
 };
